a9472d2e4cff6b85553cdba5a8e961df08ca80a6
ryuryukke <ryuto-alexander-koike@keio.jp>
2022-07-31 17:43:48 +0900

    trivial

--- src/bert_binary_classifier.py
+++ src/bert_binary_classifier.py
@@ -8,12 +8,14 @@ class BertBinaryClassifier(nn.Module):
         self.bert_model = BertModel.from_pretrained(cfg["MODEL_PARAM"]["type"])
         bert_conf = BertConfig(cfg["MODEL_PARAM"]["type"])
         self.fc1 = nn.Linear(bert_conf.hidden_size, bert_conf.hidden_size)
-        self.fc2 = nn.Linear(bert_conf.hidden_size, cfg["MODEL_PATH"]["n_label"])
+        self.fc2 = nn.Linear(bert_conf.hidden_size, cfg["MODEL_PARAM"]["n_label"])
         self.relu = nn.ReLU()
 
     def forward(self, ids, mask):
-        _, h = self.bert_model(ids, attention_mask=mask)
-        h = self.relu(h)
+        outputs = self.bert_model(ids, attention_mask=mask)
+        last_hidden_state = outputs[0]
+        cls = last_hidden_state[:, 0, :]
+        h = self.relu(cls)
         h = self.fc1(h)
         h = self.relu(h)
         logit = self.fc2(h)
