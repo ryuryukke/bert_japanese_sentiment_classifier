5f84a94fc9b4a63ebdcd306fd9d0eb2febdb57d1
ryuryukke <ryuto-alexander-koike@keio.jp>
2022-07-31 17:42:18 +0900

    add loading pre-trained model

--- src/trainer.py
+++ src/trainer.py
@@ -1,4 +1,3 @@
-import math
 import torch
 import torch.nn as nn
 from torch.optim import Adam
@@ -8,13 +7,14 @@ from bert_binary_classifier import BertBinaryClassifier
 import datetime
 import subprocess
 import numpy as np
+from preprocessor import Preprocessor
 
 
 class Trainer:
     def __init__(self, config_path: str):
         self.cfg_path = config_path
         self.cfg = self._load_config()
-        self.device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = BertBinaryClassifier(self.cfg)
         self.model.to(self.device)
         self.epoch = self.cfg["TRAIN_PARAM"]["n_epoch"]
@@ -26,6 +26,12 @@ class Trainer:
             cfg = yaml.safe_load(f)
         return cfg
 
+    def _load_pretrained_model(self, pretrained_model_path):
+        model = BertBinaryClassifier(self.cfg)
+        model.load_state_dict(torch.load(pretrained_model_path))
+        model.eval()
+        return model
+
     def _train_loop(self, train_dataloader):
         losses = []
         self.model.train()
@@ -34,7 +40,6 @@ class Trainer:
             input_ids = batch_dict["input_ids"].to(self.device)
             attention_mask = batch_dict["attention_mask"].to(self.device)
             targets = batch_dict["target"].to(self.device)
-
             logits = self.model(input_ids, attention_mask)
             loss = self.criterion(logits, targets)
             loss.backward()
@@ -45,8 +50,8 @@ class Trainer:
             losses.append(loss.item())
         return {"loss": np.array(losses).mean()}
 
-    def _test_loop(self, test_dataloader):
-        losses, predicts, targets = [], [], []
+    def _test_loop(self, test_dataloader, load_pretrained_model=False, pretrained_model_path=None):
+        losses, outputs, all_targets = [], [], []
         self.model.eval()
 
         for _, batch_dict in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
@@ -55,23 +60,32 @@ class Trainer:
             targets = batch_dict["target"].to(self.device)
 
             with torch.no_grad():
-                logits = self.model(input_ids, attention_mask)
+                if load_pretrained_model:
+                    pretrained_model = self._load_pretrained_model(pretrained_model_path)
+                    logits = pretrained_model(input_ids, attention_mask)
+                else:
+                    logits = self.model(input_ids, attention_mask)
+
+            # from IPython.core.debugger import Pdb
+
+            # Pdb().set_trace()
 
             loss = self.criterion(logits, targets)
             losses.append(loss.item())
-            targets += targets.cpu().tolist()
-            predicts += logits.sigmoid().cpu().tolist()
 
-        accuracy = self._cal_accuracy(targets, predicts)
+            all_targets += targets.cpu().tolist()
+            outputs += logits.sigmoid().cpu().tolist()
+
+        accuracy = self._cal_accuracy(targets, outputs)
 
         return {"loss": np.array(losses).mean(), "accuracy": accuracy}
 
-    def _cal_accuracy(self, targets, predicts):
-        assert len(targets) == len(predicts), "教師ラベルと予測ラベルの数が不一致です。"
+    def _cal_accuracy(self, targets, outputs):
+        assert len(targets) == len(outputs), "教師ラベルと予測ラベルの数が不一致です。"
         match_cnt = 0
-        for target, predict in zip(targets, predicts):
-            preds = np.where(np.array(predict) < 0.5, 0.0, 1.0)
-            if target == preds.tolist():
+        for target, output in zip(targets, outputs):
+            pred = np.where(np.array(output) < 0.5, 0.0, 1.0)
+            if target == pred.tolist():
                 match_cnt += 1
         return match_cnt / len(targets)
 
@@ -95,3 +109,11 @@ class Trainer:
             test_res = self._test_loop(test_dataloader)
             print(f"epoch {epoch + 1}: valid_loss = {test_res['loss']}, accuracy = {test_res['accuracy']}")
         return
+
+
+preprocessor = Preprocessor("../config/config.yaml")
+train_dataloader, valid_dataloader, test_dataloader = preprocessor()
+trainer = Trainer("../config/config.yaml")
+# trainer(train_dataloader, valid_dataloader, test_dataloader)
+
+trainer._test_loop(train_dataloader, True, "../model/2022_7_31_4_31/epoch_1.pth")
